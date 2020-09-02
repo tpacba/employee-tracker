b96132a79fc1dee460b0c018b1a89b12301ace4c
Tristan Pacba <tpacba@live.com>
2020-09-02 17:14:32 -0500

    added start function for inquirer to ask the user to add, view, or update, added options for add and view for roles, employees, or departments, added query request to insert for addDepartments function

---
+++ employeeTracker.js
@@ -0,0 +1,111 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "OneYear_95",
+    database: "employeeTracker_DB"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "add_view_update",
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Add departments, roles, employees", "View departments, roles, employees", "Update employee roles", "Exit"]
+        })
+        .then(function(answer) {
+            if (answer.add_view_update === "Add departments, roles, employees") {
+                add();
+            }
+            else if (answer.add_view_update === "View departments, roles, employees") {
+                view();
+            }
+            else if (answer.add_view_update === "Update employee roles") {
+                update();
+            }
+            else {
+                connection.end();
+            }
+        })
+}
+
+function add() {
+    inquirer
+        .prompt({
+            name: "addWhat",
+            type: "list",
+            message: "What would you like to add?",
+            choices: ["Departments", "Roles", "Employees"]
+        }).then(function(answer) {
+            if (answer.addWhat === "Departments") {
+                addDepartments();
+            }
+            else if (answer.addWhat === "Roles") {
+                addRoles();
+            }
+            else if (answer.addWhat === "Employees") {
+                addEmployees();
+            }
+            else {
+                connection.end();
+            }
+        })
+}
+
+function view() {
+    inquirer
+        .prompt({
+            name: "viewWhat",
+            type: "list",
+            message: "What would you like to view?",
+            choices: ["Departments", "Roles", "Employees"]
+        }).then(function(answer) {
+            if (answer.addWhat === "Departments") {
+                viewDepartments();
+            }
+            else if (answer.addWhat === "Roles") {
+                viewRoles();
+            }
+            else if (answer.addWhat === "Employees") {
+                viewEmployees();
+            }
+            else {
+                connection.end();
+            }
+        })
+}
+
+function update() {
+
+}
+
+function addDepartments() {
+    inquirer
+        .prompt({
+            name: "name",
+            type: "input",
+            message: "What is the name of the department (to add)?"
+        })
+        .then(function(answer) {
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    name: answer.name
+                },
+                function(err) {
+                    if (err) throw err;
+                    console.log(`You added the department ${answer.name} successfully!`)
+                    start();
+                }
+            )
+        })
+}
